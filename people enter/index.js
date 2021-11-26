b764b1967e35b5f3e254426d3b9573753617c051
Umesha Ramanayake <umesha.r@omobio.net>
2021-11-26 11:01:37 +0530

    task1 completed

--- people enter/index.js
+++ people enter/index.js
@@ -1,8 +1,18 @@
-//document.getElementById("count-el").innerText = 5
-
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
 
 
 function increment() {
-    console.log("The button was clicked")
+    count += 1
+    countEl.innerText = count
 
+
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    countEl.textContent = 0
+    couunt = 0
 }
